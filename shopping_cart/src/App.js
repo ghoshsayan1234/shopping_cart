31493b2b2454cfe8682e9d3172a774c987aebe8f
Sayan Ghosh <ghoshsayan46296@gmail.com>
2024-10-22 23:23:26 +0530

    name change

--- shopping_cart/src/App.js
+++ shopping_cart/src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-         This is Suman Changes 
+          This is Sayan Changes 
         </a>
       </header>
     </div>
