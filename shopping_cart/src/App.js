f7f8933eb59fbfe6825b698a072386b838e915ce
Sayan Ghosh <ghoshsayan46296@gmail.com>
2024-10-23 14:30:43 +0530

    popular, item

--- shopping_cart/src/App.js
+++ shopping_cart/src/App.js
@@ -1,5 +1,10 @@
 import './App.css';
 import Navbar from './components/navbar/Navbar';
+import Shop from './pages/Shop.jsx';
+import Product from './pages/Product.jsx';
+import ShopCategory from './pages/ShopCategory.jsx';
+import Cart from './pages/Cart.jsx';
+import LoginSignup from './pages/LoginSignup.jsx';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 function App() {
@@ -8,7 +13,15 @@ function App() {
       <BrowserRouter>
       <Navbar />
       <Routes>
-        
+        <Route path='/' element={<Shop/>}/>
+        <Route path='/mens' element={<ShopCategory category="mens"/>}/>
+        <Route path='/womens' element={<ShopCategory category="womens"/>}/>
+        <Route path='/kids' element={<ShopCategory category="kids"/>}/>
+        <Route path="product" element={<Product />}>
+          <Route path=":productId" element={<Product />}/>
+        </Route>
+        <Route path='/cart' element={<Cart/>}/>
+        <Route path='/login' element={<LoginSignup/>}/>
       </Routes>
       </BrowserRouter>
     </div>
--- shopping_cart/src/pages/ShopCategory.jsx
+++ shopping_cart/src/pages/ShopCategory.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 
-const ShopCatagory = () => {
+const ShopCategory = () => {
   return (
     <div>
       
@@ -8,4 +8,4 @@ const ShopCatagory = () => {
   )
 }
 
-export default ShopCatagory
+export default ShopCategory;
